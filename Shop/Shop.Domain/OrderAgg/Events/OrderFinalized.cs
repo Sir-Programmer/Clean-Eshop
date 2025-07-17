b697e5c6fa39f98b1560a641b21c8c3982dcbfff
Mojtaba Karamlou <contact@karamlou.com>
2025-07-17 21:32:39 +0330

    Add OrderFinalized event to order aggregation

---
+++ Shop/Shop.Domain/OrderAgg/Events/OrderFinalized.cs
@@ -0,0 +1,8 @@
+﻿using Common.Domain;
+
+namespace Shop.Domain.OrderAgg.Events;
+
+public class OrderFinalized(Guid orderId) : BaseDomainEvent
+{
+    public Guid OrderId { get; private set; } = orderId;
+}
