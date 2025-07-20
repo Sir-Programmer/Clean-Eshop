3f8da2d0cac2f0b689aab2d6b7074b9432c2849c
Mojtaba Karamlou <contact@karamlou.com>
2025-07-21 01:27:10 +0330

    Add Role and Permission classes to manage user roles and permissions

---
+++ Shop/Shop.Domain/RoleAgg/Role.cs
@@ -0,0 +1,32 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace Shop.Domain.RoleAgg;
+
+public class Role : AggregateRoot
+{
+    public Role(string title)
+    {
+        Guard(title);
+        Title = title;
+        Permissions = [];
+    }
+    public string Title { get; private set; }
+    public List<RolePermission> Permissions { get; private set; }
+
+    public void Edit(string title)
+    {
+        Guard(title);
+        Title = title;
+    }
+
+    public void SetPermissions(List<RolePermission> permissions)
+    {
+        Permissions = permissions;
+    }
+
+    private void Guard(string title)
+    {
+        NullOrEmptyDomainException.CheckString(title, nameof(title));
+    }
+}
---
+++ Shop/Shop.Domain/RoleAgg/RolePermission.cs
@@ -0,0 +1,10 @@
+﻿using Common.Domain;
+using Shop.Domain.RoleAgg.Enums;
+
+namespace Shop.Domain.RoleAgg;
+
+public class RolePermission(Permission permission) : BaseEntity
+{
+    public Guid RoleId { get; internal set; }
+    public Permission Permission { get; private set; } = permission;
+}
