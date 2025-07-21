5a927fc1c67818f4caab3b41f3e70aa7ba61b70f
Mojtaba Karamlou <contact@karamlou.com>
2025-07-21 23:59:48 +0330

    Add Category aggregate root with child management and slug validation

---
+++ Shop/Shop.Domain/CategoryAgg/Category.cs
@@ -0,0 +1,48 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Common.Domain.Utils;
+using Common.Domain.ValueObjects;
+using Shop.Domain.CategoryAgg.Services;
+
+namespace Shop.Domain.CategoryAgg;
+
+public class Category : AggregateRoot
+{
+    public Category(string title, string slug, SeoData seoData, ICategoryDomainService categoryDomainService)
+    {
+        Guard(title, slug, categoryDomainService);
+        Title = title;
+        Slug = slug.ToSlug();
+        SeoData = seoData;
+        Childs = [];
+    }
+    public string Title { get; private set; }
+    public string Slug { get; private set; }
+    public SeoData SeoData { get; private set; }
+    public Guid? ParentId { get; private set; }
+    public List<Category> Childs { get; private set; }
+
+    public void Edit(string title, string slug, SeoData seoData, ICategoryDomainService categoryDomainService)
+    {
+        Guard(title, slug, categoryDomainService);
+        Title = title;
+        Slug = slug.ToSlug();
+        SeoData = seoData;
+    }
+    
+    public void AddChild(string title, string slug, SeoData seoData, ICategoryDomainService categoryDomainService)
+    {
+        Childs.Add(new Category(title, slug, seoData, categoryDomainService)
+        {
+            ParentId = Id
+        });
+    }
+    private void Guard(string title, string slug, ICategoryDomainService categoryDomainService)
+    {
+        NullOrEmptyDomainException.CheckString(title, nameof(title));
+        NullOrEmptyDomainException.CheckString(slug, nameof(slug));
+        if (Slug != slug.ToSlug())
+            if (categoryDomainService.IsSlugExist(slug.ToSlug()))
+                throw new SlugDuplicatedException();
+    }
+}
