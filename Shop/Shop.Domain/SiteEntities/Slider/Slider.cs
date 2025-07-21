8962e0a12ba5b9e6f842abcada89aed6bfeb74de
Mojtaba Karamlou <contact@karamlou.com>
2025-07-22 00:19:07 +0330

    Add Slider entity with validation and activation management

---
+++ Shop/Shop.Domain/SiteEntities/Slider/Slider.cs
@@ -0,0 +1,44 @@
+﻿using Common.Domain.Exceptions;
+
+namespace Shop.Domain.SiteEntities.Slider;
+
+public class Slider
+{
+    public Slider(string title, string url, string imageName)
+    {
+        Guard(title, url, imageName);
+        Title = title;
+        Url = url;
+        ImageName = imageName;
+        IsActive = true;
+    }
+    public string Title { get; private set; }
+    public string Url { get; private set; }
+    public string ImageName { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public void Edit(string title, string url, string imageName, bool isActive)
+    {
+        Title = title;
+        Url = url;
+        ImageName = imageName;
+        IsActive = isActive;
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+    }
+
+    public void DeActivate()
+    {
+        IsActive = false;
+    }
+
+    private void Guard(string title, string url, string imageName)
+    {
+        NullOrEmptyDomainException.CheckString(title, nameof(title));
+        NullOrEmptyDomainException.CheckString(url, nameof(url));
+        NullOrEmptyDomainException.CheckString(imageName, nameof(imageName));
+    }
+}
