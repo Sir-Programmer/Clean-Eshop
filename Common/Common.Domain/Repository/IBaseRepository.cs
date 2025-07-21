d5d5642c962c7347622b6656344f04f28de342d8
Mojtaba Karamlou <contact@karamlou.com>
2025-07-22 00:57:21 +0330

    Add IBaseRepository interface for generic repository operations

---
+++ Common/Common.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+
+namespace Common.Domain.Repository;
+
+public interface IBaseRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(long id);
+    Task<T?> GetByIdTrackingAsync(long id);
+
+    Task<List<T>> GetAllAsync();
+
+    Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task<T?> FindOneAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+
+    void Update(T entity);
+
+    void Remove(T entity);
+    Task RemoveAsync(T entity);
+
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    
+    Task<(List<T> Items, int TotalCount)> GetPagedAsync(
+        int pageNumber,
+        int pageSize,
+        Expression<Func<T, bool>>? filter = null,
+        Expression<Func<T, object>>? orderBy = null,
+        bool orderByDescending = false);
+}
+
