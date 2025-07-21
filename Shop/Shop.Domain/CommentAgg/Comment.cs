85723581c9f6fc9f1d35385a319f1c75d41c0d3d
Mojtaba Karamlou <contact@karamlou.com>
2025-07-22 00:11:39 +0330

    Add Comment aggregate root with status management and validation

---
+++ Shop/Shop.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,40 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Shop.Domain.CommentAgg.Enums;
+
+namespace Shop.Domain.CommentAgg;
+
+public class Comment : AggregateRoot
+{
+    public Comment(Guid userId, string text, Guid productId, CommentStatus status, DateTime lastUpdate)
+    {
+        Guard(text);
+        UserId = userId;
+        Text = text;
+        ProductId = productId;
+        Status = CommentStatus.Pending;
+    }
+    public Guid UserId { get; private set; }
+    public string Text { get; private set; }
+    public Guid ProductId { get; private set; }
+    public CommentStatus Status { get; private set; }
+    public DateTime LastUpdate { get; private set; }
+
+    public void Edit(string text)
+    {
+        Guard(text);
+        Text = text;
+        Status = CommentStatus.Pending;
+    }
+
+    public void ChangeStatus(CommentStatus status)
+    {
+        Status = status;
+        LastUpdate = DateTime.Now;
+    }
+
+    private void Guard(string text)
+    {
+        NullOrEmptyDomainException.CheckString(text, nameof(text));
+    }
+}
