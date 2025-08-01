36a8e4e13e5d544cca8a307d5211cefad6f4332f
Mojtaba Karamlou <contact@karamlou.com>
2025-08-01 22:22:16 +0330

    Add SetUserActiveAddress command and handler

---
+++ Shop/Shop.Application/Users/SetActiveAddress/SetUserActiveAddressCommand.cs
@@ -0,0 +1,5 @@
+﻿using Common.Application;
+
+namespace Shop.Application.Users.SetActiveAddress;
+
+public record SetUserActiveAddressCommand(Guid UserId, Guid AddressId) : IBaseCommand;
---
+++ Shop/Shop.Application/Users/SetActiveAddress/SetUserActiveAddressCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Common.Application;
+using Common.Application.OperationResults;
+using Common.Application.UnitOfWork;
+using Shop.Domain.UserAgg.Repository;
+
+namespace Shop.Application.Users.SetActiveAddress;
+
+public class SetUserActiveAddressCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork) : IBaseCommandHandler<SetUserActiveAddressCommand>
+{
+    public async Task<OperationResult> Handle(SetUserActiveAddressCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetByIdTrackingAsync(request.UserId);
+        if (user == null)
+            return OperationResult.NotFound();
+        user.SetActiveAddress(request.AddressId);
+        
+        await unitOfWork.SaveChangesAsync();
+        return OperationResult.Success();
+    }
+}
