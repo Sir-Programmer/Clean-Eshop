2e06319ae1d961d9e910da7edfa126e48e295566
Mojtaba Karamlou <contact@karamlou.com>
2025-08-01 22:19:17 +0330

    Add DeleteUserAddress command and handler

---
+++ Shop/Shop.Application/Users/DeleteAddress/DeleteUserAddressCommand.cs
@@ -0,0 +1,5 @@
+﻿using Common.Application;
+
+namespace Shop.Application.Users.DeleteAddress;
+
+public record DeleteUserAddressCommand(Guid UserId, Guid AddressId) : IBaseCommand;
---
+++ Shop/Shop.Application/Users/DeleteAddress/DeleteUserAddressCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Common.Application;
+using Common.Application.OperationResults;
+using Common.Application.UnitOfWork;
+using Shop.Domain.UserAgg.Repository;
+
+namespace Shop.Application.Users.DeleteAddress;
+
+public class DeleteUserAddressCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork) : IBaseCommandHandler<DeleteUserAddressCommand>
+{
+    public async Task<OperationResult> Handle(DeleteUserAddressCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetByIdTrackingAsync(request.UserId);
+        if (user == null)
+            return OperationResult.NotFound();
+        user.RemoveAddress(request.AddressId);
+
+        await unitOfWork.SaveChangesAsync();
+        return OperationResult.Success();
+    }
+}
