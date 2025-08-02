9506e8d8d80289e48cb7e88ba12ee7a8caac1e24
Mojtaba Karamlou <contact@karamlou.com>
2025-08-03 01:05:45 +0330

    Add OrderRepository implementation with method to retrieve current user order

---
+++ Shop/Shop.Infrastructure/Persistent.Ef/OrderAgg/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Common.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.OrderAgg;
+using Shop.Domain.OrderAgg.Enums;
+using Shop.Domain.OrderAgg.Repository;
+using Shop.Infrastructure._Utilities;
+
+namespace Shop.Infrastructure.Persistent.Ef.OrderAgg;
+
+public class OrderRepository(ShopContext context) : BaseRepository<Order>(context), IOrderRepository
+{
+    public async Task<Order?> GetCurrentUserOrder(Guid userId)
+    {
+        return await Context.Orders.AsTracking().FirstOrDefaultAsync(o => o.UserId == userId && o.Status == OrderStatus.Created);
+    }
+}
