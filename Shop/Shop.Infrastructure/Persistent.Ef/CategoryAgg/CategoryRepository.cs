022194fc96474880a1e17590b7632ff6c08e2f52
Mojtaba Karamlou <contact@karamlou.com>
2025-08-04 01:35:52 +0330

    Implement DeleteCategory method in CategoryRepository with cascading delete logic

--- Shop/Shop.Infrastructure/Persistent.Ef/CategoryAgg/CategoryRepository.cs
+++ Shop/Shop.Infrastructure/Persistent.Ef/CategoryAgg/CategoryRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Shop.Domain.CategoryAgg;
 using Shop.Domain.CategoryAgg.Repository;
 using Shop.Infrastructure._Utilities;
@@ -6,8 +7,28 @@ namespace Shop.Infrastructure.Persistent.Ef.CategoryAgg;
 
 public class CategoryRepository(ShopContext context) : BaseRepository<Category>(context), ICategoryRepository
 {
-    public Task<bool> DeleteCategory(Guid categoryId)
+    public async Task<bool> DeleteCategory(Guid categoryId)
     {
-        throw new NotImplementedException();
+        var category = await context.Categories
+            .Include(c => c.Childs)
+            .ThenInclude(c => c.Childs)
+            .FirstOrDefaultAsync(c => c.Id == categoryId);
+        
+        if (category == null) return false;
+        
+        var existProduct = await Context.Products
+            .Include(c => c.CategoryIds)
+            .AnyAsync(p => p.CategoryIds.Contains(categoryId));
+        
+        if (existProduct) return false;
+
+        if (category.Childs.Any(c => c.Childs.Count != 0))
+        {
+            Context.RemoveRange(category.Childs.SelectMany(s=>s.Childs));
+        }
+        
+        Context.RemoveRange(category.Childs);
+        Context.RemoveRange(category);
+        return true;
     }
 }
