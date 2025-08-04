c53104f633fc0f66cea69e8c6b4db38ce96f709d
Mojtaba Karamlou <contact@karamlou.com>
2025-08-05 02:11:56 +0330

    Add CommentConfiguration for EF Core with indexing and validation

---
+++ Shop/Shop.Infrastructure/Persistent.Ef/CommentAgg/CommentConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.CommentAgg;
+
+namespace Shop.Infrastructure.Persistent.Ef.CommentAgg;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.ToTable("Comments", "comment");
+        builder.HasIndex(c => c.ProductId);
+        builder.HasIndex(c => c.UserId);
+        
+        builder.Property(comment => comment.Text)
+            .IsRequired()
+            .HasMaxLength(500);
+    }
+}
