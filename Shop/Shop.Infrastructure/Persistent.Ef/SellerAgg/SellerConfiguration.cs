7c07b3ad926329ed734f356811970c9fd987f0db
Mojtaba Karamlou <contact@karamlou.com>
2025-08-05 13:25:43 +0330

    Add SellerConfiguration and SellerRepository for seller management

---
+++ Shop/Shop.Infrastructure/Persistent.Ef/SellerAgg/SellerConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.SellerAgg;
+
+namespace Shop.Infrastructure.Persistent.Ef.SellerAgg;
+
+public class SellerConfiguration : IEntityTypeConfiguration<Seller>
+{
+    public void Configure(EntityTypeBuilder<Seller> builder)
+    {
+        builder.ToTable("Sellers", "seller");
+        builder.HasIndex(s => s.NationalId).IsUnique();
+
+        builder.Property(s => s.NationalId)
+            .IsRequired()
+            .HasMaxLength(10);
+        
+        builder.Property(s => s.ShopName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.OwnsMany(s => s.Inventories, option =>
+        {
+            option.ToTable("Inventories", "seller");
+
+            option.HasIndex(i => i.ProductId);
+            option.HasIndex(i => i.SellerId);
+        });
+    }
+}
---
+++ Shop/Shop.Infrastructure/Persistent.Ef/SellerAgg/SellerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Domain.SellerAgg;
+using Shop.Domain.SellerAgg.Repository;
+using Shop.Infrastructure._Utilities;
+
+namespace Shop.Infrastructure.Persistent.Ef.SellerAgg;
+
+public class SellerRepository(ShopContext context) : BaseRepository<Seller>(context), ISellerRepository
+{
+    public Task<InventoryResult?> GetInventoryById(Guid inventoryId)
+    {
+        //TODO : Use DAPPER
+        throw new NotImplementedException();
+    }
+}
