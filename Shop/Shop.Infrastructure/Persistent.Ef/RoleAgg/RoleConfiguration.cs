9c1272d2b4582e896ddff0e41eac340644de9d60
Mojtaba Karamlou <contact@karamlou.com>
2025-08-05 12:54:38 +0330

    Add RoleConfiguration and RoleRepository for role management

---
+++ Shop/Shop.Infrastructure/Persistent.Ef/RoleAgg/RoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.RoleAgg;
+
+namespace Shop.Infrastructure.Persistent.Ef.RoleAgg;
+
+public class RoleConfiguration :  IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Roles", "role");
+
+        builder.Property(r => r.Title)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.OwnsMany(r => r.Permissions, option =>
+        {
+            option.ToTable("Permissions", "role");
+        });
+    }
+}
---
+++ Shop/Shop.Infrastructure/Persistent.Ef/RoleAgg/RoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Shop.Domain.RoleAgg;
+using Shop.Domain.RoleAgg.Repository;
+using Shop.Infrastructure._Utilities;
+
+namespace Shop.Infrastructure.Persistent.Ef.RoleAgg;
+
+public class RoleRepository(ShopContext context) : BaseRepository<Role>(context), IRoleRepository
+{
+    
+}
