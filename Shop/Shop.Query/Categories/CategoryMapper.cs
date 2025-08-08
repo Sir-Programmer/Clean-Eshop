e41cef67cc2df0a8bf6a0eaf8b40f73a863333df
Mojtaba Karamlou <contact@karamlou.com>
2025-08-08 19:52:04 +0330

    Add DTOs and mapping for category queries

---
+++ Shop/Shop.Query/Categories/CategoryMapper.cs
@@ -0,0 +1,41 @@
+﻿using Shop.Domain.CategoryAgg;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories;
+
+internal static class CategoryMapper
+{
+    public static CategoryDto? Map(this Category? category)
+    {
+        if (category == null)
+            return null;
+
+        return new CategoryDto
+        {
+            Title = category.Title,
+            Slug = category.Slug,
+            Id = category.Id,
+            SeoData = category.SeoData,
+            CreationTime = category.CreationTime,
+            Childs = category.Childs.MapChildren()
+        };
+    }
+
+    public static List<CategoryDto?> Map(this List<Category> categories)
+    {
+        return categories.Select(c => c.Map()).ToList();
+    }
+
+    private static List<CategoryDto> MapChildren(this List<Category> children)
+    {
+        return children.Select(c => new CategoryDto
+        {
+            Title = c.Title,
+            Slug = c.Slug,
+            Id = c.Id,
+            SeoData = c.SeoData,
+            CreationTime = c.CreationTime,
+            Childs = c.Childs.MapChildren()
+        }).ToList();
+    }
+}
---
+++ Shop/Shop.Query/Categories/GetById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetById;
+
+public class GetCategoryByIdQueryHandler(ShopContext context) : IQueryHandler<GetCategoryByIdQuery, CategoryDto?>
+{
+    public async Task<CategoryDto?> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == request.CategoryId, cancellationToken);
+        return category.Map();
+    }
+}
