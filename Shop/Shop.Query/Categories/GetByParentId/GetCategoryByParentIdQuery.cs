cdb37dc0e85b0cd5c34e94c152938ee39ce1c455
Mojtaba Karamlou <contact@karamlou.com>
2025-08-08 19:59:16 +0330

    Add query and handler for retrieving categories by parent ID

---
+++ Shop/Shop.Query/Categories/GetByParentId/GetCategoryByParentIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Common.Query;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetByParentId;
+
+public record GetCategoryByParentIdQuery(Guid ParentId) : IQuery<CategoryDto?>;
---
+++ Shop/Shop.Query/Categories/GetByParentId/GetCategoryByParentIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetByParentId;
+
+public class GetCategoryByParentIdQueryHandler(ShopContext context) : IQueryHandler<GetCategoryByParentIdQuery, CategoryDto?>
+{
+    public async Task<CategoryDto?> Handle(GetCategoryByParentIdQuery request, CancellationToken cancellationToken)
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(c => c.ParentId == request.ParentId, cancellationToken: cancellationToken);
+        return category.Map();
+    }
+}
