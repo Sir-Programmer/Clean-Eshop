e8409d8aebb384fc0ad55c0576174ec0d80a1ab7
Mojtaba Karamlou <contact@karamlou.com>
2025-08-08 20:30:57 +0330

    Add query and handler for retrieving category list

---
+++ Shop/Shop.Query/Categories/GetList/GetCategoryListQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetList;
+
+public class GetCategoryListQueryHandler(ShopContext context) : IQueryHandler<GetCategoryListQuery, List<CategoryDto?>>
+{
+    public async Task<List<CategoryDto?>> Handle(GetCategoryListQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await context.Categories
+            .Where(c => c.ParentId == null)
+            .Include(c => c.Childs)
+            .ThenInclude(c => c.Childs)
+            .ToListAsync(cancellationToken: cancellationToken);
+        return categories.Map();
+    }
+}
