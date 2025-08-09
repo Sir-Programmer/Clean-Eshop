5abd3c1307e9b976e059cfb2eb148c41b7186d24
Mojtaba Karamlou <contact@karamlou.com>
2025-08-10 00:44:22 +0330

    Add GetCommentByIdQuery and handler for retrieving comments by ID

---
+++ Shop/Shop.Query/Comments/GetById/GetCommentByIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Comments.DTOs;
+
+namespace Shop.Query.Comments.GetById;
+
+public class GetCommentByIdQueryHandler(ShopContext context) : IQueryHandler<GetCommentByIdQuery, CommentDto?>
+{
+    public async Task<CommentDto?> Handle(GetCommentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var comment = await context.Comments.FirstOrDefaultAsync(c => c.Id == request.CommentId, cancellationToken: cancellationToken);
+        return comment.Map();
+    }
+}
