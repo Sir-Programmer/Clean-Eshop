4e62456299a658ed1bbd1307699af7a6ab83daf9
Mojtaba Karamlou <contact@karamlou.com>
2025-08-11 00:47:33 +0330

    Add domain services for Category, Product, Seller, and User with placeholder methods

---
+++ Shop/Shop.Application/Products/ProductDomainService.cs
@@ -0,0 +1,11 @@
+﻿using Shop.Domain.ProductAgg.Services;
+
+namespace Shop.Application.Products;
+
+public class ProductDomainService : IProductDomainService
+{
+    public bool IsSlugExist(string slug)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Shop/Shop.Application/Sellers/SellerDomainService.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.SellerAgg.Services;
+
+namespace Shop.Application.Sellers;
+
+public class SellerDomainService : ISellerDomainService
+{
+    public bool IsNationalIdExist(string nationalId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsUserIdExist(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
