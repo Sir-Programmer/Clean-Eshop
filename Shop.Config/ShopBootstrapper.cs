de2cfc514c5e0bfcaedbdfa827bbfa6073d32299
Mojtaba Karamlou <contact@karamlou.com>
2025-08-11 00:42:27 +0330

    Add initialization method to ShopBootstrapper for service registration

--- Shop.Config/ShopBootstrapper.cs
+++ Shop.Config/ShopBootstrapper.cs
@@ -1,5 +1,20 @@
-﻿namespace Shop.Config;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Categories.Create;
+using Shop.Infrastructure;
+using Shop.Query.Categories.GetById;
+
+namespace Shop.Config;
 
 public static class ShopBootstrapper
 {
+    public static void Initialize(this IServiceCollection services, string connectionString)
+    {
+        InfrastructureBootstrapper.Initialize(services, connectionString);
+        
+        services.AddMediatR(option =>
+        {
+            option.RegisterServicesFromAssembly(typeof(CreateCategoryCommand).Assembly);
+            option.RegisterServicesFromAssembly(typeof(GetCategoryByIdQuery).Assembly);
+        });
+    }
 }
