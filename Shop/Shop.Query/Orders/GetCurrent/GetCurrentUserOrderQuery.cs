6294996747035b8805ce107434daa83470e2145a
Mojtaba Karamlou <contact@karamlou.com>
2025-08-11 20:18:41 +0330

    Add GetCurrentUserOrderQuery and handler for retrieving user orders

---
+++ Shop/Shop.Query/Orders/GetCurrent/GetCurrentUserOrderQuery.cs
@@ -0,0 +1,6 @@
+﻿using Common.Query;
+using Shop.Query.Orders.DTOs;
+
+namespace Shop.Query.Orders.GetCurrent;
+
+public record GetCurrentUserOrderQuery(Guid UserId) : IQuery<OrderDto?>;
---
+++ Shop/Shop.Query/Orders/GetCurrent/GetCurrentUserOrderQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Dapper;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Orders.DTOs;
+
+namespace Shop.Query.Orders.GetCurrent;
+
+public class GetCurrentUserOrderQueryHandler(ShopContext context, DapperContext dapperContext) : IQueryHandler<GetCurrentUserOrderQuery, OrderDto?>
+{
+    public async Task<OrderDto?> Handle(GetCurrentUserOrderQuery request, CancellationToken cancellationToken)
+    {
+        var order = await context.Orders.SingleOrDefaultAsync(o => o.UserId == request.UserId, cancellationToken: cancellationToken);
+        var orderDto = order.Map(context);
+        if (orderDto == null) return null;
+        orderDto.Items = await orderDto.GetOrderItems(dapperContext);
+        return orderDto;
+    }
+}
