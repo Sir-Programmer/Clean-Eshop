5f32e76970725e57c9cce65caa08d9d090547183
Mojtaba Karamlou <contact@karamlou.com>
2025-08-16 14:15:40 +0330

    Add GetProductByFilterQuery and handler for product filtering

---
+++ Shop/Shop.Query/Products/GetByFilter/GetProductByFilterQuery.cs
@@ -0,0 +1,7 @@
+﻿using Common.Query;
+using Shop.Domain.ProductAgg;
+using Shop.Query.Products.DTOs.Filter;
+
+namespace Shop.Query.Products.GetByFilter;
+
+public class GetProductByFilterQuery(ProductFilterParams filterParams) : QueryFilter<ProductFilterResult, ProductFilterParams>(filterParams);
---
+++ Shop/Shop.Query/Products/GetByFilter/GetProductByFilterQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Common.Query;
+using Common.Query.Filter;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Products.DTOs.Filter;
+
+namespace Shop.Query.Products.GetByFilter;
+
+public class GetProductByFilterQueryHandler(ShopContext context) : IQueryHandler<GetProductByFilterQuery, ProductFilterResult>
+{
+    public Task<ProductFilterResult> Handle(GetProductByFilterQuery request, CancellationToken cancellationToken)
+    {
+        var @params = request.FilterParams;
+        var query = context.Products.OrderByDescending(p => p.CreationTime).AsQueryable();
+        
+        if (@params.Title !=  null)
+            query = query.Where(p => p.Title.Contains(@params.Title));
+        if (@params.Slug !=  null)
+            query = query.Where(p => p.Slug.ToLower().Contains(@params.Slug.ToLower()));
+
+        var data = query.Select(p => p.MapFilter()).ToSafePagedList(@params.PageId, @params.Take).ToList();
+        var result = new ProductFilterResult()
+        {
+            Data = data,
+            FilterParams = @params
+        };
+        
+        result.GeneratePaging(query.Count(), @params.Take, @params.PageId);
+
+        return Task.FromResult(result);
+    }
+}
