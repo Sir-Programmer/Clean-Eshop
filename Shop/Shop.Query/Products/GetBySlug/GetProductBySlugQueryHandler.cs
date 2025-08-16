733b5304bd5adfa70f63aa52ae95e2bdd9c12bcd
Mojtaba Karamlou <contact@karamlou.com>
2025-08-16 15:35:20 +0330

    Add GetProductBySlugQuery and handler for retrieving product details by slug

---
+++ Shop/Shop.Query/Products/GetBySlug/GetProductBySlugQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using Common.Query;
+using Shop.Query.Products.DTOs;
+using Shop.Query.Products.Services;
+
+namespace Shop.Query.Products.GetBySlug;
+
+public class GetProductBySlugQueryHandler(IProductQueryService productQueryService) : IQueryHandler<GetProductBySlugQuery, ProductDto?>
+{
+    public async Task<ProductDto?> Handle(GetProductBySlugQuery request, CancellationToken cancellationToken) =>
+        await productQueryService.GetBySlugAsync(request.Slug, cancellationToken: cancellationToken);
+}
