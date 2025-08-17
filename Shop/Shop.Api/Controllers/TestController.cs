0d8e08b1558b4098fd8a4f55a551949c2bfa8303
Mojtaba Karamlou <contact@karamlou.com>
2025-08-17 04:03:18 +0330

    Product Test

--- Shop/Shop.Api/Controllers/TestController.cs
+++ Shop/Shop.Api/Controllers/TestController.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Application.Users.Register;
 using Shop.Query.Orders.GetById;
+using Shop.Query.Products.GetById;
 
 namespace Shop.Api.Controllers;
 
@@ -9,10 +10,17 @@ namespace Shop.Api.Controllers;
 [ApiController]
 public class TestController(IMediator mediator) : ControllerBase
 {
-    [HttpGet]
-    public async Task<IActionResult> Index(Guid orderId)
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetOrder(Guid orderId)
     {
         var order = await mediator.Send(new GetOrderByIdQuery(orderId));
         return Ok(order);
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetProduct(Guid productId)
+    {
+        var product = await mediator.Send(new GetProductByIdQuery(productId));
+        return Ok(product);
     }
 }
