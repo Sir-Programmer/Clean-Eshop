8f00369add83f77160f231089dac625e21de9382
Mojtaba Karamlou <contact@karamlou.com>
2025-08-19 18:28:54 +0330

    Add GetBannerByIdQuery and handler for retrieving banner details by ID

---
+++ Shop/Shop.Query/SiteEntities/Banners/GetById/GetBannerByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Common.Query;
+using Shop.Domain.SiteEntities.Banner;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Query.SiteEntities.Banners.GetById;
+
+public record GetBannerByIdQuery(Guid BannerId) : IQuery<BannerDto?>;
---
+++ Shop/Shop.Query/SiteEntities/Banners/GetById/GetBannerByIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Query.SiteEntities.Banners.GetById;
+
+public class GetBannerByIdQueryHandler(ShopContext context) : IQueryHandler<GetBannerByIdQuery, BannerDto?>
+{
+    public async Task<BannerDto?> Handle(GetBannerByIdQuery request, CancellationToken cancellationToken)
+    {
+        var banner = await context.Banners.FirstOrDefaultAsync(b => b.Id == request.BannerId, cancellationToken: cancellationToken);
+        return banner.MapBanner();
+    }
+}
