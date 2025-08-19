3f7a111a62afc0b998d3c990af19ce737cf6cf4a
Mojtaba Karamlou <contact@karamlou.com>
2025-08-19 18:57:27 +0330

    Add GetShippingMethodListQuery and handler for retrieving a list of shipping methods

---
+++ Shop/Shop.Query/SiteEntities/ShippingMethods/GetList/GetShippingMethodListQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Query.SiteEntities.ShippingMethods.GetList;
+
+public class GetShippingMethodListQueryHandler(ShopContext context) : IQueryHandler<GetShippingMethodListQuery, List<ShippingMethodDto?>>
+{
+    public async Task<List<ShippingMethodDto?>> Handle(GetShippingMethodListQuery request, CancellationToken cancellationToken)
+    {
+        return await context.ShippingMethods.Select(s => s.MapShippingMethod()).ToListAsync(cancellationToken);
+    }
+}
