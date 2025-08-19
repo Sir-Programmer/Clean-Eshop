9a5288ad0544552d4da7ffabc704d19cd00eb548
Mojtaba Karamlou <contact@karamlou.com>
2025-08-19 18:25:55 +0330

    Add BannerDto class for banner data representation

---
+++ Shop/Shop.Query/SiteEntities/DTOs/BannerDto.cs
@@ -0,0 +1,11 @@
+﻿using Common.Query;
+using Shop.Domain.SiteEntities.Banner.Enums;
+
+namespace Shop.Query.SiteEntities.DTOs;
+
+public class BannerDto : BaseDto
+{
+    public string Url { get; set; }
+    public string ImageName { get; set; }
+    public BannerPosition BannerPosition { get; set; }
+}
