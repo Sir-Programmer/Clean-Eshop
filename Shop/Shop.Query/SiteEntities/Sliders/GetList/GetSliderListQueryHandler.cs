b003e4fe4d609b0c54725838a8cddf446e9200ae
Mojtaba Karamlou <contact@karamlou.com>
2025-08-19 18:25:05 +0330

    Add GetSliderListQuery and handler for retrieving a list of sliders

---
+++ Shop/Shop.Query/SiteEntities/Sliders/GetList/GetSliderListQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Query.SiteEntities.Sliders.GetList;
+
+public class GetSliderListQueryHandler(ShopContext context) : IQueryHandler<GetSliderListQuery, List<SliderDto?>>
+{
+    public async Task<List<SliderDto?>> Handle(GetSliderListQuery request, CancellationToken cancellationToken)
+    {
+        return  await context.Sliders.Select(s => s.MapSlider()).ToListAsync(cancellationToken: cancellationToken);
+    }
+}
