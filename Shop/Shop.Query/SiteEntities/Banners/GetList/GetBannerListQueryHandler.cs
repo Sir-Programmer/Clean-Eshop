9b07a02618e96000b079728edc729c881d3bfa25
Mojtaba Karamlou <contact@karamlou.com>
2025-08-19 18:38:53 +0330

    Add GetBannerListQuery and handler for retrieving a list of banners

---
+++ Shop/Shop.Query/SiteEntities/Banners/GetList/GetBannerListQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Query.SiteEntities.Banners.GetList;
+
+public class GetBannerListQueryHandler (ShopContext context) : IQueryHandler<GetBannerListQuery, List<BannerDto?>>
+{
+    public async Task<List<BannerDto?>> Handle(GetBannerListQuery request, CancellationToken cancellationToken)
+    {
+        return await context.Banners.Select(b => b.MapBanner()).ToListAsync(cancellationToken);
+    }
+}
