621ae167c1a1e31be757c207591918101bd27778
Mojtaba Karamlou <contact@karamlou.com>
2025-08-21 19:37:05 +0330

    Add SellerDto class for seller data representation

---
+++ Shop/Shop.Query/Sellers/DTOs/SellerDto.cs
@@ -0,0 +1,12 @@
+﻿using Common.Query;
+using Shop.Domain.SellerAgg.Enums;
+
+namespace Shop.Query.Sellers.DTOs;
+
+public class SellerDto : BaseDto
+{
+    public Guid UserId { get; set; }
+    public string ShopName { get; set; }
+    public string NationalId { get; set; }
+    public SellerStatus Status { get; set; }
+}
