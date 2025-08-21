ed6e838c46789a46c0b0ead68b1fe13e10bd9729
Mojtaba Karamlou <contact@karamlou.com>
2025-08-21 19:45:40 +0330

    Add SellerFilterParams and SellerFilterResult classes for seller filtering

---
+++ Shop/Shop.Query/Sellers/DTOs/Filter/SellerFilterParams.cs
@@ -0,0 +1,9 @@
+﻿using Common.Query.Filter;
+
+namespace Shop.Query.Sellers.DTOs.Filter;
+
+public class SellerFilterParams : BaseFilterParam
+{
+    public string ShopName { get; set; }
+    public string NationalId { get; set; }
+}
