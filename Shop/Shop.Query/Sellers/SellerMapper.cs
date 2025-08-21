b70cbcb0a838a8bb6fc1fba8c5f7f2c13e2db6f6
Mojtaba Karamlou <contact@karamlou.com>
2025-08-21 19:38:26 +0330

    Add mapping method to SellerMapper for converting Seller to SellerDto

--- Shop/Shop.Query/Sellers/SellerMapper.cs
+++ Shop/Shop.Query/Sellers/SellerMapper.cs
@@ -1,6 +1,21 @@
+using Shop.Domain.SellerAgg;
+using Shop.Query.Sellers.DTOs;
+
 namespace Shop.Query.Sellers;
 
 public static class SellerMapper
 {
-    
+    public static SellerDto? Map(this Seller? seller)
+    {
+        if  (seller == null) return null;
+        return new SellerDto()
+        {
+            Id = seller.Id,
+            CreationTime = seller.CreationTime,
+            NationalId = seller.NationalId,
+            ShopName = seller.ShopName,
+            Status = seller.Status,
+            UserId = seller.UserId
+        };
+    }
 }
