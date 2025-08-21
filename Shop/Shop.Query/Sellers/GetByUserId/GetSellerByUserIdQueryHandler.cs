06cc1055425d84cab3ef9acd165fb33436c8e8a4
Mojtaba Karamlou <contact@karamlou.com>
2025-08-21 19:42:51 +0330

    Add GetSellerByUserId query and handler for retrieving seller by user ID

---
+++ Shop/Shop.Query/Sellers/GetByUserId/GetSellerByUserIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Sellers.DTOs;
+
+namespace Shop.Query.Sellers.GetByUserId;
+
+public class GetSellerByUserIdQueryHandler(ShopContext context) :  IQueryHandler<GetSellerByUserIdQuery, SellerDto?>
+{
+    public async Task<SellerDto?> Handle(GetSellerByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var seller = await context.Sellers.FirstOrDefaultAsync(s => s.UserId == request.UserId, cancellationToken);
+        return seller.Map();
+    }
+}
