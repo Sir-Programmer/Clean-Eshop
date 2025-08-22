e8bbebd31e8fd45fbdb0df129bc382f06c4ee640
Mojtaba Karamlou <contact@karamlou.com>
2025-08-22 19:34:17 +0330

    Add SellerInventoryFilterParams and SellerInventoryFilterResult classes for inventory filtering

---
+++ Shop/Shop.Query/Sellers/DTOs/Filter/SellerInventoryFilterParams.cs
@@ -0,0 +1,8 @@
+﻿using Common.Query.Filter;
+
+namespace Shop.Query.Sellers.DTOs.Filter;
+
+public class SellerInventoryFilterParams : BaseFilterParam
+{
+    public Guid SellerId { get; set; }
+}
---
+++ Shop/Shop.Query/Sellers/DTOs/Filter/SellerInventoryFilterResult.cs
@@ -0,0 +1,5 @@
+﻿using Common.Query.Filter;
+
+namespace Shop.Query.Sellers.DTOs.Filter;
+
+public class SellerInventoryFilterResult : BaseFilter<InventoryDto, SellerInventoryFilterParams>;
