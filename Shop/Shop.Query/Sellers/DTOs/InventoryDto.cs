ac8bfbec882f8ff532bed46613bbf25214fd0141
Mojtaba Karamlou <contact@karamlou.com>
2025-08-22 18:55:05 +0330

    Add GetSellerInventoryByIdQuery and InventoryDto for retrieving seller inventory by ID

---
+++ Shop/Shop.Query/Sellers/DTOs/InventoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop.Query.Sellers.DTOs;
+
+public class InventoryDto
+{
+    public Guid ProductId { get; set; }
+    public string ProductTitle { get; set; }
+    public string ProductImage { get; set; }
+    public int Count { get; set; }
+    public int Price { get; set; }
+    public Guid SellerId { get; set; }
+    public int? DiscountPercentage { get;  set; }
+    public bool IsActive { get; set; }
+}
