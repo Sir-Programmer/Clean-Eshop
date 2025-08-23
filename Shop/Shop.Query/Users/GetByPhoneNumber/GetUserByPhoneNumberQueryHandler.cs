9e424c961dd99bd17c232b39fef027e8dacc0487
Mojtaba Karamlou <contact@karamlou.com>
2025-08-23 05:09:36 +0330

    Add GetUserByPhoneNumber query and handler for user retrieval by phone number

---
+++ Shop/Shop.Query/Users/GetByPhoneNumber/GetUserByPhoneNumberQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Users.DTOs;
+using Shop.Query.Users.Services;
+
+namespace Shop.Query.Users.GetByPhoneNumber;
+
+public class GetUserByPhoneNumberQueryHandler(ShopContext context, IUserQueryService userQueryService) : IQueryHandler<GetUserByPhoneNumberQuery, UserDto?>
+{
+    public async Task<UserDto?> Handle(GetUserByPhoneNumberQuery request, CancellationToken cancellationToken)
+    {
+        var user = await context.Users.Include(u => u.Roles).SingleOrDefaultAsync(u => u.PhoneNumber == request.PhoneNumber, cancellationToken);
+        if (user == null) return null;
+        var userRoles = await userQueryService.GetRolesDataAsync(user.Roles.Select(r => r.RoleId).ToList());
+        return user.Map(userRoles);
+    }
+}
