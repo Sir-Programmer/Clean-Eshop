cc1101551cebe6bb647fa2551c6c12bba59b50d5
Mojtaba Karamlou <contact@karamlou.com>
2025-08-23 04:24:14 +0330

    Add UserMapper for mapping User entities to UserDto

---
+++ Shop/Shop.Query/Users/UserMapper.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.UserAgg;
+using Shop.Query.Users.DTOs;
+
+namespace Shop.Query.Users;
+
+public static class UserMapper
+{
+    public static UserDto? Map(this User? user)
+    {
+        if  (user == null) return null;
+        return new UserDto()
+        {
+
+        };
+    }
+}
