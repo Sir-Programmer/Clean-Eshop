e325598459e41ae2eea2d856a071550c6749ac54
Mojtaba Karamlou <contact@karamlou.com>
2025-08-23 04:22:27 +0330

    Add DTOs for User and UserRole to improve data handling

---
+++ Shop/Shop.Query/Users/DTOs/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Domain.UserAgg.Enums;
+
+namespace Shop.Query.Users.DTOs;
+
+public class UserDto
+{
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Gender Gender { get; set; }
+
+    public List<UserRoleDto> Roles { get; set; }
+}
