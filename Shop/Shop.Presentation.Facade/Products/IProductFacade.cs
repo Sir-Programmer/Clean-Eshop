ff6f4d1654ecdfa6aa2797f3d39aa80646261150
Mojtaba Karamlou <contact@karamlou.com>
2025-08-25 01:25:05 +0330

    Add IProductFacade and ProductFacade implementations for product operations

---
+++ Shop/Shop.Presentation.Facade/Products/IProductFacade.cs
@@ -0,0 +1,24 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.Orders.Finally;
+using Shop.Application.Products.AddImage;
+using Shop.Application.Products.Create;
+using Shop.Application.Products.Edit;
+using Shop.Application.Products.EditImageSequence;
+using Shop.Application.Products.RemoveImage;
+using Shop.Query.Products.DTOs;
+using Shop.Query.Products.DTOs.Filter;
+
+namespace Shop.Presentation.Facade.Products;
+
+public interface IProductFacade
+{
+    Task<OperationResult<Guid>> Create(CreateProductCommand command);
+    Task<OperationResult> Edit(EditProductCommand command);
+    Task<OperationResult> AddImage(AddProductImageCommand command);
+    Task<OperationResult> EditImageSequence(EditProductImageSequenceCommand command);
+    Task<OperationResult> RemoveImage(RemoveProductImageCommand command);
+
+    Task<ProductFilterResult> GetByFilter(ProductFilterParams filters);
+    Task<ProductDto?> GetById(Guid id);
+    Task<ProductDto?> GetBySlug(string slug);
+}
---
+++ Shop/Shop.Presentation.Facade/Products/ProductFacade.cs
@@ -0,0 +1,57 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Products.AddImage;
+using Shop.Application.Products.Create;
+using Shop.Application.Products.Edit;
+using Shop.Application.Products.EditImageSequence;
+using Shop.Application.Products.RemoveImage;
+using Shop.Query.Products.DTOs;
+using Shop.Query.Products.DTOs.Filter;
+using Shop.Query.Products.GetByFilter;
+using Shop.Query.Products.GetById;
+using Shop.Query.Products.GetBySlug;
+
+namespace Shop.Presentation.Facade.Products;
+
+public class ProductFacade(IMediator mediator) : IProductFacade
+{
+    public async Task<OperationResult<Guid>> Create(CreateProductCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditProductCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> AddImage(AddProductImageCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> EditImageSequence(EditProductImageSequenceCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> RemoveImage(RemoveProductImageCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<ProductFilterResult> GetByFilter(ProductFilterParams filters)
+    {
+        return await mediator.Send(new GetProductByFilterQuery(filters));
+    }
+
+    public async Task<ProductDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetProductByIdQuery(id));
+    }
+
+    public async Task<ProductDto?> GetBySlug(string slug)
+    {
+        return await mediator.Send(new GetProductBySlugQuery(slug));
+    }
+}
