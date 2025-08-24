e0f7ed3957428175a460b0d4a7fd8be0077df812
Mojtaba Karamlou <contact@karamlou.com>
2025-08-25 00:52:13 +0330

    Add IOrderFacade and OrderFacade implementations for order operations

---
+++ Shop/Shop.Presentation.Facade/Orders/IOrderFacade.cs
@@ -0,0 +1,28 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.Orders.AddItem;
+using Shop.Application.Orders.Checkout;
+using Shop.Application.Orders.DecreaseItemCount;
+using Shop.Application.Orders.Finally;
+using Shop.Application.Orders.IncreaseItemCount;
+using Shop.Application.Orders.RemoveItem;
+using Shop.Application.Orders.SendOrder;
+using Shop.Query.Orders.DTOs;
+using Shop.Query.Orders.DTOs.Filter;
+
+namespace Shop.Presentation.Facade.Orders;
+
+public interface IOrderFacade
+{
+    Task<OperationResult> AddItem(AddOrderItemCommand command);
+    Task<OperationResult> RemoveItem(RemoveOrderItemCommand command);
+    Task<OperationResult> Checkout(CheckoutOrderCommand command);
+    Task<OperationResult> DecreaseItemCount(DecreaseOrderItemCountCommand command);
+    Task<OperationResult> IncreaseItemCount(IncreaseOrderItemCountCommand command);
+    Task<OperationResult> Finally(FinallyOrderCommand command);
+    Task<OperationResult> Send(SendOrderCommand command);
+
+    
+    Task<OrderFilterResult> GetByFilter(OrderFilterParams filters);
+    Task<OrderDto?> GetById(Guid id);
+    Task<OrderDto?> GetCurrentUserOrder(Guid userId);
+}
---
+++ Shop/Shop.Presentation.Facade/Orders/OrderFacade.cs
@@ -0,0 +1,69 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Orders.AddItem;
+using Shop.Application.Orders.Checkout;
+using Shop.Application.Orders.DecreaseItemCount;
+using Shop.Application.Orders.Finally;
+using Shop.Application.Orders.IncreaseItemCount;
+using Shop.Application.Orders.RemoveItem;
+using Shop.Application.Orders.SendOrder;
+using Shop.Query.Orders.DTOs;
+using Shop.Query.Orders.DTOs.Filter;
+using Shop.Query.Orders.GetByFilter;
+using Shop.Query.Orders.GetById;
+using Shop.Query.Orders.GetCurrent;
+
+namespace Shop.Presentation.Facade.Orders;
+
+public class OrderFacade(IMediator mediator) : IOrderFacade
+{
+    public async Task<OperationResult> AddItem(AddOrderItemCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> RemoveItem(RemoveOrderItemCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Checkout(CheckoutOrderCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> DecreaseItemCount(DecreaseOrderItemCountCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> IncreaseItemCount(IncreaseOrderItemCountCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Finally(FinallyOrderCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Send(SendOrderCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OrderFilterResult> GetByFilter(OrderFilterParams filters)
+    {
+        return await mediator.Send(new GetOrderByFilterQuery(filters));
+    }
+
+    public async Task<OrderDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetOrderByIdQuery(id));
+    }
+
+    public async Task<OrderDto?> GetCurrentUserOrder(Guid userId)
+    {
+        return await mediator.Send(new GetCurrentUserOrderQuery(userId));
+    }
+}
