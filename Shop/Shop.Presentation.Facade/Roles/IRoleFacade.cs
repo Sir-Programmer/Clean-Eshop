8d588c4b98af9f041256ce2c3d0226c6884bb7d9
Mojtaba Karamlou <contact@karamlou.com>
2025-08-26 01:17:50 +0330

    Add IRoleFacade and RoleFacade implementations for role operations

---
+++ Shop/Shop.Presentation.Facade/Roles/IRoleFacade.cs
@@ -0,0 +1,15 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.Roles.Create;
+using Shop.Application.Roles.Edit;
+using Shop.Query.Roles.DTOs;
+
+namespace Shop.Presentation.Facade.Roles;
+
+public interface IRoleFacade
+{
+    Task<OperationResult<Guid>> Create(CreateRoleCommand command);
+    Task<OperationResult> Edit(EditRoleCommand command);
+    
+    Task<RoleDto?> GetById(Guid id);
+    Task<List<RoleDto>> GetAll();
+}
---
+++ Shop/Shop.Presentation.Facade/Roles/RoleFacade.cs
@@ -0,0 +1,32 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Roles.Create;
+using Shop.Application.Roles.Edit;
+using Shop.Query.Roles.DTOs;
+using Shop.Query.Roles.GetById;
+using Shop.Query.Roles.GetList;
+
+namespace Shop.Presentation.Facade.Roles;
+
+public class RoleFacade(IMediator mediator) : IRoleFacade
+{
+    public async Task<OperationResult<Guid>> Create(CreateRoleCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditRoleCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<RoleDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetRoleByIdQuery(id));
+    }
+
+    public async Task<List<RoleDto>> GetAll()
+    {
+        return await mediator.Send(new GetRoleListQuery());
+    }
+}
