83ad57a0e6226496fb91bbc048f38619dc51041e
Mojtaba Karamlou <contact@karamlou.com>
2025-08-27 19:47:37 +0330

    Add ISellerFacade and SellerFacade implementations for seller operations

---
+++ Shop/Shop.Presentation.Facade/Sellers/ISellerFacade.cs
@@ -0,0 +1,17 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.Sellers.Create;
+using Shop.Application.Sellers.Edit;
+using Shop.Query.Sellers.DTOs;
+using Shop.Query.Sellers.DTOs.Filter;
+
+namespace Shop.Presentation.Facade.Sellers;
+
+public interface ISellerFacade
+{
+    Task<OperationResult<Guid>> Create(CreateSellerCommand command);
+    Task<OperationResult> Edit(EditSellerCommand command);
+    
+    Task<SellerDto?> GetById(Guid id);
+    Task<SellerDto?> GetByUserId(Guid userId);
+    Task<SellerFilterResult> GetByFilter(SellerFilterParams filters);
+}
---
+++ Shop/Shop.Presentation.Facade/Sellers/SellerFacade.cs
@@ -0,0 +1,39 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Sellers.Create;
+using Shop.Application.Sellers.Edit;
+using Shop.Query.Sellers.DTOs;
+using Shop.Query.Sellers.DTOs.Filter;
+using Shop.Query.Sellers.GetByFilter;
+using Shop.Query.Sellers.GetById;
+using Shop.Query.Sellers.GetByUserId;
+
+namespace Shop.Presentation.Facade.Sellers;
+
+public class SellerFacade(IMediator mediator) : ISellerFacade
+{
+    public async Task<OperationResult<Guid>> Create(CreateSellerCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditSellerCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<SellerDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetSellerByIdQuery(id));
+    }
+
+    public async Task<SellerDto?> GetByUserId(Guid userId)
+    {
+        return await mediator.Send(new GetSellerByUserIdQuery(userId));
+    }
+
+    public async Task<SellerFilterResult> GetByFilter(SellerFilterParams filters)
+    {
+        return await mediator.Send(new GetSellerByFilterQuery(filters));
+    }
+}
