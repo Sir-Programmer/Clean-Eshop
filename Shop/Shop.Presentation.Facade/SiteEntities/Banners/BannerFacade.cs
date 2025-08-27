0ffe0f59d4dc86771d3b332a0b415f8f7001436c
Mojtaba Karamlou <contact@karamlou.com>
2025-08-27 19:57:40 +0330

    Add IBannerFacade and BannerFacade implementations for banner operations

---
+++ Shop/Shop.Presentation.Facade/SiteEntities/Banners/BannerFacade.cs
@@ -0,0 +1,38 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.SiteEntities.Banners.Create;
+using Shop.Application.SiteEntities.Banners.Delete;
+using Shop.Application.SiteEntities.Banners.Edit;
+using Shop.Query.SiteEntities.Banners.GetById;
+using Shop.Query.SiteEntities.Banners.GetList;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Presentation.Facade.SiteEntities.Banners;
+
+public class BannerFacade(IMediator mediator) : IBannerFacade
+{
+    public async Task<OperationResult<Guid>> Create(CreateBannerCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditBannerCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Delete(DeleteBannerCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<BannerDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetBannerByIdQuery(id));
+    }
+
+    public async Task<List<BannerDto>> GetList()
+    {
+        return await mediator.Send(new GetBannerListQuery());
+    }
+}
---
+++ Shop/Shop.Presentation.Facade/SiteEntities/Banners/IBannerFacade.cs
@@ -0,0 +1,17 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.SiteEntities.Banners.Create;
+using Shop.Application.SiteEntities.Banners.Delete;
+using Shop.Application.SiteEntities.Banners.Edit;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Presentation.Facade.SiteEntities.Banners;
+
+public interface IBannerFacade
+{
+    Task<OperationResult<Guid>> Create(CreateBannerCommand command);
+    Task<OperationResult> Edit(EditBannerCommand command);
+    Task<OperationResult> Delete(DeleteBannerCommand command);
+    
+    Task<BannerDto?> GetById(Guid id);
+    Task<List<BannerDto>> GetList();
+}
