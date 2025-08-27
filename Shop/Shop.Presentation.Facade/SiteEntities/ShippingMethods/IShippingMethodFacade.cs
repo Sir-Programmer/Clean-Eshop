a538fcf68e36dfc097e6e6ec4e3190c51c8ccd8d
Mojtaba Karamlou <contact@karamlou.com>
2025-08-27 20:00:53 +0330

    Add IShippingMethodFacade and ShippingMethodFacade implementations for shipping method operations

---
+++ Shop/Shop.Presentation.Facade/SiteEntities/ShippingMethods/IShippingMethodFacade.cs
@@ -0,0 +1,17 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.SiteEntities.ShippingMethods.Create;
+using Shop.Application.SiteEntities.ShippingMethods.Delete;
+using Shop.Application.SiteEntities.ShippingMethods.Edit;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Presentation.Facade.SiteEntities.ShippingMethods;
+
+public interface IShippingMethodFacade
+{
+    Task<OperationResult<Guid>> Create(CreateShippingMethodCommand command);
+    Task<OperationResult> Edit(EditShippingMethodCommand command);
+    Task<OperationResult> Delete(DeleteShippingMethodCommand command);
+    
+    Task<ShippingMethodDto?> GetById(Guid id);
+    Task<List<ShippingMethodDto>> GetList();
+}
---
+++ Shop/Shop.Presentation.Facade/SiteEntities/ShippingMethods/ShippingMethodFacade.cs
@@ -0,0 +1,38 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.SiteEntities.ShippingMethods.Create;
+using Shop.Application.SiteEntities.ShippingMethods.Delete;
+using Shop.Application.SiteEntities.ShippingMethods.Edit;
+using Shop.Query.SiteEntities.DTOs;
+using Shop.Query.SiteEntities.ShippingMethods.GetById;
+using Shop.Query.SiteEntities.ShippingMethods.GetList;
+
+namespace Shop.Presentation.Facade.SiteEntities.ShippingMethods;
+
+public class ShippingMethodFacade(IMediator mediator) : IShippingMethodFacade
+{
+    public async Task<OperationResult<Guid>> Create(CreateShippingMethodCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditShippingMethodCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Delete(DeleteShippingMethodCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<ShippingMethodDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetShippingMethodByIdQuery(id));
+    }
+
+    public async Task<List<ShippingMethodDto>> GetList()
+    {
+        return await mediator.Send(new GetShippingMethodListQuery());
+    }
+}
