b1c2f624add9c1e33ead2e27b725a4ee833102eb
Mojtaba Karamlou <contact@karamlou.com>
2025-08-27 20:02:32 +0330

    Add ISliderFacade and SliderFacade implementations for slider operations

---
+++ Shop/Shop.Presentation.Facade/SiteEntities/Sliders/ISliderFacade.cs
@@ -0,0 +1,17 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.SiteEntities.Sliders.Create;
+using Shop.Application.SiteEntities.Sliders.Delete;
+using Shop.Application.SiteEntities.Sliders.Edit;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Presentation.Facade.SiteEntities.Sliders;
+
+public interface ISliderFacade
+{
+    Task<OperationResult<Guid>> Create(CreateSliderCommand command);
+    Task<OperationResult> Edit(EditSliderCommand command);
+    Task<OperationResult> Delete(DeleteSliderCommand command);
+    
+    Task<SliderDto?> GetById(Guid id);
+    Task<List<SliderDto>> GetList();
+}
---
+++ Shop/Shop.Presentation.Facade/SiteEntities/Sliders/SliderFacade.cs
@@ -0,0 +1,38 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.SiteEntities.Sliders.Create;
+using Shop.Application.SiteEntities.Sliders.Delete;
+using Shop.Application.SiteEntities.Sliders.Edit;
+using Shop.Query.SiteEntities.DTOs;
+using Shop.Query.SiteEntities.Sliders.GetById;
+using Shop.Query.SiteEntities.Sliders.GetList;
+
+namespace Shop.Presentation.Facade.SiteEntities.Sliders;
+
+public class SliderFacade(IMediator mediator) : ISliderFacade
+{
+    public async Task<OperationResult<Guid>> Create(CreateSliderCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditSliderCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Delete(DeleteSliderCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<SliderDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetSliderByIdQuery(id));
+    }
+
+    public async Task<List<SliderDto>> GetList()
+    {
+        return await mediator.Send(new GetSliderListQuery());
+    }
+}
