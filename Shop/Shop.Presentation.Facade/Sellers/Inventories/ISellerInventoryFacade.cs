5f5567b430d06ef5794b11ba3d65e68ee760d92f
Mojtaba Karamlou <contact@karamlou.com>
2025-08-27 19:53:02 +0330

    Add ISellerInventoryFacade and SellerInventoryFacade implementations for inventory operations

---
+++ Shop/Shop.Presentation.Facade/Sellers/Inventories/ISellerInventoryFacade.cs
@@ -0,0 +1,17 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.Sellers.AddInventory;
+using Shop.Application.Sellers.EditInventory;
+using Shop.Query.Sellers.DTOs;
+using Shop.Query.Sellers.DTOs.Filter;
+
+namespace Shop.Presentation.Facade.Sellers.Inventories;
+
+public interface ISellerInventoryFacade
+{
+    Task<OperationResult> AddInventory(AddSellerInventoryCommand command);
+    Task<OperationResult> EditInventory(EditSellerInventoryCommand command);
+    
+    Task<InventoryDto?> GetById(Guid id);
+    Task<InventoryDto?> GetByProductId(Guid productId);
+    Task<SellerInventoryFilterResult> GetByFilter(SellerInventoryFilterParams filters);
+}
---
+++ Shop/Shop.Presentation.Facade/Sellers/Inventories/SellerInventoryFacade.cs
@@ -0,0 +1,39 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Sellers.AddInventory;
+using Shop.Application.Sellers.EditInventory;
+using Shop.Query.Sellers.DTOs;
+using Shop.Query.Sellers.DTOs.Filter;
+using Shop.Query.Sellers.Inventories.GetByFilter;
+using Shop.Query.Sellers.Inventories.GetById;
+using Shop.Query.Sellers.Inventories.GetByProductId;
+
+namespace Shop.Presentation.Facade.Sellers.Inventories;
+
+public class SellerInventoryFacade(IMediator mediator) : ISellerInventoryFacade
+{
+    public async Task<OperationResult> AddInventory(AddSellerInventoryCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> EditInventory(EditSellerInventoryCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<InventoryDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetSellerInventoryByIdQuery(id));
+    }
+
+    public async Task<InventoryDto?> GetByProductId(Guid productId)
+    {
+        return await mediator.Send(new GetSellerInventoryByProductIdQuery(productId));
+    }
+
+    public async Task<SellerInventoryFilterResult> GetByFilter(SellerInventoryFilterParams filters)
+    {
+        return await mediator.Send(new GetSellerInventoryByFilterQuery(filters));
+    }
+}
