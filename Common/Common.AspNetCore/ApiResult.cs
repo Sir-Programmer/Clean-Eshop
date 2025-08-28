960c5d43ed4ec507f0f31af02eba311c8d34fe61
Mojtaba Karamlou <contact@karamlou.com>
2025-08-28 23:04:55 +0330

    Add ApiResult class and related types for standardized API responses

---
+++ Common/Common.AspNetCore/ApiResult.cs
@@ -0,0 +1,31 @@
+﻿namespace Common.AspNetCore;
+
+public class ApiResult
+{
+    public bool IsSuccess { get; set; }
+    public required MetaData MetaData { get; set; }
+}
+
+public class ApiResult<TData>
+{
+    public bool IsSuccess { get; set; }
+    public required TData Data { get; set; }
+    public required MetaData MetaData { get; set; }
+}
+
+public class MetaData
+{
+    public OperationStatusCode OperationStatusCode { get; set; }
+    public required string Message { get; set; }
+}
+
+
+public enum OperationStatusCode
+{
+    Success = 1000,
+    NotFound = 1001,
+    BadRequest = 1002,
+    LogicError = 1003,
+    UnAuthorize = 1004,
+    ServerError = 1005
+}
