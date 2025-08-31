5e818b4bc6be94c57cba974c2471458a8cf779ac
Mojtaba Karamlou <contact@karamlou.com>
2025-09-01 03:00:58 +0330

    Add IVerificationFacade interface and implementation for code verification operations

---
+++ Shop/Shop.Presentation.Facade/Verifications/IVerificationFacade.cs
@@ -0,0 +1,11 @@
+﻿using Common.Application.OperationResults;
+using Shop.Application.Verifications.RequestCode;
+using Shop.Application.Verifications.VerifyCode;
+
+namespace Shop.Presentation.Facade.Verifications;
+
+public interface IVerificationFacade
+{
+    Task<OperationResult> RequestCode(RequestCodeCommand command);
+    Task<OperationResult> VerifyCode(VerifyCodeCommand command);
+}
---
+++ Shop/Shop.Presentation.Facade/Verifications/VerificationFacade.cs
@@ -0,0 +1,19 @@
+﻿using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Verifications.RequestCode;
+using Shop.Application.Verifications.VerifyCode;
+
+namespace Shop.Presentation.Facade.Verifications;
+
+public class VerificationFacade(IMediator mediator) : IVerificationFacade
+{
+    public async Task<OperationResult> RequestCode(RequestCodeCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> VerifyCode(VerifyCodeCommand command)
+    {
+        return await mediator.Send(command);
+    }
+}
