b713c23dab8b881cae2e465cc33be9e291f97eb5
Mojtaba Karamlou <contact@karamlou.com>
2025-08-31 23:40:27 +0330

    Add PhoneNumber and Password fields to LoginViewModel and RegisterViewModel with validation attributes

--- Shop/Shop.Api/ViewModels/Auth/LoginViewModel.cs
+++ Shop/Shop.Api/ViewModels/Auth/LoginViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Shop.Api.ViewModels.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Api.ViewModels.Auth;
 
 public class LoginViewModel
 {
+    [Display(Name = "شماره موبایل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [Length(11, 11, ErrorMessage = "{0} نا معتبر است")]
+    public required string PhoneNumber { get; set; }
     
+    [Display(Name = "کلمه عبور")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MinLength(6, ErrorMessage = "{0} نمیتواند کمتر از {1} کارکتر داشته باشد")]
+    [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} کارکتر باشد")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
 }
--- Shop/Shop.Api/ViewModels/Auth/RegisterViewModel.cs
+++ Shop/Shop.Api/ViewModels/Auth/RegisterViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace Shop.Api.ViewModels.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Api.ViewModels.Auth;
 
 public class RegisterViewModel
 {
+    [Display(Name = "شماره موبایل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [Length(11, 11, ErrorMessage = "{0} نا معتبر است")]
+    public required string PhoneNumber { get; set; }
     
+    [Display(Name = "کلمه عبور")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MinLength(6, ErrorMessage = "{0} نمیتواند کمتر از {1} کارکتر داشته باشد")]
+    [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} کارکتر باشد")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    
+    [Display(Name = "تکرار کلمه عبور")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MinLength(6, ErrorMessage = "{0} نمیتواند کمتر از {1} کارکتر داشته باشد")]
+    [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} کارکتر باشد")]
+    [Compare("Password", ErrorMessage = "کلمه عبور و تکرار آن با هم مغایرت دارند")]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
 }
