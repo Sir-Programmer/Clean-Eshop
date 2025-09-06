5d17ae4c1f984643fbc18534ceb5d6b084026339
Mojtaba Karamlou <contact@karamlou.com>
2025-09-06 20:11:36 +0330

    Add command and handler for adding user tokens

---
+++ Shop/Shop.Application/Users/AddToken/AddUserTokenCommand.cs
@@ -0,0 +1,5 @@
+﻿using Common.Application;
+
+namespace Shop.Application.Users.AddToken;
+
+public record AddUserTokenCommand(Guid UserId, string TokenHash, string RefreshTokenHash, DateTime TokenExpireDate, DateTime RefreshTokenExpireDate, string Device) : IBaseCommand;
---
+++ Shop/Shop.Application/Users/AddToken/AddUserTokenCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Common.Application;
+using Common.Application.OperationResults;
+using Common.Application.UnitOfWork;
+using Shop.Domain.UserAgg.Repository;
+
+namespace Shop.Application.Users.AddToken;
+
+public class AddUserTokenCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork) : IBaseCommandHandler<AddUserTokenCommand>
+{
+    public async Task<OperationResult> Handle(AddUserTokenCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetByIdAsync(request.UserId);
+        if  (user == null)
+            return OperationResult.NotFound();
+        user.AddToken(request.TokenHash, request.RefreshTokenHash, request.TokenExpireDate, request.RefreshTokenExpireDate, request.Device);
+        await unitOfWork.SaveChangesAsync();
+        return OperationResult.Success();
+    }
+}
