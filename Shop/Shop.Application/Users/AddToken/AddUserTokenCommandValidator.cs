5f32ca4876e5fcbdcf541d22260eba54e1c93808
Mojtaba Karamlou <contact@karamlou.com>
2025-09-06 20:13:51 +0330

    Add validator for AddUserToken command to enforce token requirements

---
+++ Shop/Shop.Application/Users/AddToken/AddUserTokenCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using Common.Application.Validation;
+using FluentValidation;
+
+namespace Shop.Application.Users.AddToken;
+
+public class AddUserTokenCommandValidator : AbstractValidator<AddUserTokenCommand>
+{
+    public AddUserTokenCommandValidator()
+    {
+        RuleFor(x => x.TokenHash)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.Required("توکن"));
+        
+        RuleFor(x => x.RefreshTokenHash)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.Required("رفرش توکن"));
+        
+        RuleFor(x => x.Device)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.Required("اطلاعات دستگاه"));
+    }
+}
