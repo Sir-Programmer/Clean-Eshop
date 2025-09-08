b71a08704616ca310cf601bba8a582c541e0a85c
Mojtaba Karamlou <contact@karamlou.com>
2025-09-09 01:25:04 +0330

    Add ClaimUtils class for retrieving user claims from ClaimsPrincipal

---
+++ Common/Common.AspNetCore/ClaimUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace Common.AspNetCore;
+
+public static class ClaimUtils
+{
+    public static Guid? GetUserId(this ClaimsPrincipal principal)
+    {
+        return Guid.TryParse(principal?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var id) ? id : null;
+    }
+
+    public static string? GetPhoneNumber(this ClaimsPrincipal principal)
+    {
+        return principal?.FindFirst(ClaimTypes.MobilePhone)?.Value;
+    }
+}
