cc0d003b2ddc2ddf4ce0688eb203f46f8b65c95c
Mojtaba Karamlou <contact@karamlou.com>
2025-09-09 01:28:53 +0330

    Add custom JWT validation to enhance token validation process

---
+++ Shop/Shop.Api/Infrastructure/JwtUtils/CustomJwtValidation.cs
@@ -0,0 +1,18 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Shop.Presentation.Facade.Users;
+
+namespace Shop.Api.Infrastructure.JwtUtils;
+
+public class CustomJwtValidation(IUserFacade userFacade)
+{
+    public async Task Validate(TokenValidatedContext context)
+    {
+        var jwtToken = context.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+        var token = await userFacade.GetByJwtToken(jwtToken);
+        if (token == null)
+        {
+            context.Fail("Invalid Token");
+        }
+    }
+}
