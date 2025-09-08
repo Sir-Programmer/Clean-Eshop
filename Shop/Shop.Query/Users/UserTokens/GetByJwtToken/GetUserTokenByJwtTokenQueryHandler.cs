68a707db0e89e5b89243ee80ff5a478a3a2ed7aa
Mojtaba Karamlou <contact@karamlou.com>
2025-09-08 05:02:38 +0330

    Add GetUserTokenByJwtToken query and handler for token retrieval by JWT

---
+++ Shop/Shop.Query/Users/UserTokens/GetByJwtToken/GetUserTokenByJwtTokenQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Common.Query;
+using Dapper;
+using Shop.Infrastructure.Persistent.Dapper;
+using Shop.Query.Users.DTOs;
+
+namespace Shop.Query.Users.UserTokens.GetByJwtToken;
+
+public class GetUserTokenByJwtTokenQueryHandler(DapperContext dapperContext) :  IQueryHandler<GetUserTokenByJwtTokenQuery, UserTokenDto?>
+{
+    public async Task<UserTokenDto?> Handle(GetUserTokenByJwtTokenQuery request, CancellationToken cancellationToken)
+    {
+        var connection = dapperContext.CreateConnection();
+        const string sql = $"SELECT TOP(1) * FROM {DapperContext.UserTokens} WHERE TokenHash = @JwtToken";
+        return await connection.QueryFirstOrDefaultAsync<UserTokenDto>(sql, new { request.JwtToken });
+    }
+}
