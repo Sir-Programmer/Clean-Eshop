b16b7ee042aeef4fc7ec5ce6031ec95c41687057
Mojtaba Karamlou <contact@karamlou.com>
2025-09-11 03:18:28 +0330

    Add ProductShopDto, ProductShopFilterParams, and ProductShopResult classes for product filtering

---
+++ Shop/Shop.Query/Products/DTOs/Filter/ProductShopFilterParams.cs
@@ -0,0 +1,19 @@
+﻿using Common.Query.Filter;
+
+namespace Shop.Query.Products.DTOs.Filter;
+
+public class ProductShopFilterParams : BaseFilterParam
+{
+    public string? CategorySlug { get; set; } = "";
+    public string? Search { get; set; } = "";
+    public bool OnlyAvailableProducts { get; set; } = false;
+    public bool JustHasDiscount { get; set; } = false;
+    public ProductSearchOrderBy SearchOrderBy { get; set; } = ProductSearchOrderBy.Cheapest;
+}
+
+public enum ProductSearchOrderBy
+{
+    Latest,
+    Expensive,
+    Cheapest,
+}
---
+++ Shop/Shop.Query/Products/DTOs/Filter/ProductShopResult.cs
@@ -0,0 +1,9 @@
+﻿using Common.Query.Filter;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Products.DTOs.Filter;
+
+public class ProductShopResult : BaseFilter<ProductShopDto, ProductShopFilterParams>
+{
+    public CategoryDto? CategoryDto { get; set; }
+}
