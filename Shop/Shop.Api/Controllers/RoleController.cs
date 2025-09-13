1c82a74a7e05fd320866e39c9c41873a3b2d8dd3
Mojtaba Karamlou <contact@karamlou.com>
2025-09-13 20:25:40 +0330

    Add EditRoleViewModel and enhance RoleController with CRUD operations

--- Shop/Shop.Api/Controllers/RoleController.cs
+++ Shop/Shop.Api/Controllers/RoleController.cs
@@ -1,8 +1,40 @@
 ﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Api.ViewModels.Role;
+using Shop.Application.Roles.Create;
+using Shop.Application.Roles.Edit;
+using Shop.Presentation.Facade.Roles;
+using Shop.Query.Roles.DTOs;
 
 namespace Shop.Api.Controllers;
 
-public class RoleController : ApiController
+public class RoleController(IRoleFacade roleFacade) : ApiController
 {
+    [HttpGet]
+    public async Task<ApiResult<List<RoleDto>>> GetList()
+    {
+        var result = await roleFacade.GetAll();
+        return QueryResult(result);
+    }
     
+    [HttpGet("{id:guid}")]
+    public async Task<ApiResult<RoleDto?>> GetById(Guid id)
+    {
+        var result = await roleFacade.GetById(id);
+        return QueryResult(result);
+    }
+
+    [HttpPost]
+    public async Task<ApiResult<Guid>> Create(CreateRoleCommand command)
+    {
+        var result = await roleFacade.Create(command);
+        return CommandResult(result);
+    }
+    
+    [HttpPut("{id:guid}")]
+    public async Task<ApiResult> Edit(Guid id, EditRoleViewModel vm)
+    {
+        var result = await roleFacade.Edit(new EditRoleCommand(id, vm.Title, vm.Permissions));
+        return CommandResult(result);
+    }
 }
---
+++ Shop/Shop.Api/ViewModels/Role/EditRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Domain.RoleAgg.Enums;
+
+namespace Shop.Api.ViewModels.Role;
+
+public class EditRoleViewModel
+{
+    public string Title { get; set; }
+    public List<Permission> Permissions { get; set; }
+}
