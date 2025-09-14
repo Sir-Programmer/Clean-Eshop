e39f47badd7b2180c567ee1f1df631cce5823e5e
Mojtaba Karamlou <contact@karamlou.com>
2025-09-14 13:28:51 +0330

    Add SliderController to manage slider entities; implement GetList and GetById methods

---
+++ Shop/Shop.Api/Controllers/SliderController.cs
@@ -0,0 +1,23 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Presentation.Facade.SiteEntities.Sliders;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Api.Controllers;
+
+public class SliderController(ISliderFacade sliderFacade) : ApiController
+{
+    [HttpGet]
+    public async Task<ApiResult<List<SliderDto>>> GetList()
+    {
+        var result = await sliderFacade.GetList();
+        return QueryResult(result);
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<ApiResult<SliderDto?>> GetById(Guid id)
+    {
+        var result = await sliderFacade.GetById(id);
+        return QueryResult(result);
+    }
+}
