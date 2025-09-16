044035e9a8d30cb9f455ad5898a72239b7949d6d
Mojtaba Karamlou <contact@karamlou.com>
2025-09-16 20:35:07 +0330

    Add methods to SellerController for retrieving sellers by filter, ID, and user ID

--- Shop/Shop.Api/Controllers/SellerController.cs
+++ Shop/Shop.Api/Controllers/SellerController.cs
@@ -1,8 +1,30 @@
 ﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Presentation.Facade.Sellers;
+using Shop.Query.Sellers.DTOs;
+using Shop.Query.Sellers.DTOs.Filter;
 
 namespace Shop.Api.Controllers;
 
-public class SellerController : ApiController
+public class SellerController(ISellerFacade sellerFacade) : ApiController
 {
+    [HttpGet]
+    public async Task<ApiResult<SellerFilterResult?>> GetByFilter([FromQuery]  SellerFilterParams filters)
+    {
+        var result = await sellerFacade.GetByFilter(filters);
+        return QueryResult(result);
+    }
+    [HttpGet("{id:guid}")]
+    public async Task<ApiResult<SellerDto?>> GetById(Guid id)
+    {
+        var result = await sellerFacade.GetById(id);
+        return QueryResult(result);
+    }
     
+    [HttpGet("by-user/{userId:guid}")]
+    public async Task<ApiResult<SellerDto?>> GetByUserId(Guid userId)
+    {
+        var result = await sellerFacade.GetByUserId(userId);
+        return QueryResult(result);
+    }
 }
