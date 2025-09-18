52bd18d7e15f40a3e2efd05c85dff7326145ab2e
Mojtaba Karamlou <contact@karamlou.com>
2025-09-18 18:34:52 +0330

    Add user creation and editing functionality
    Refactored `UserController` to include dependency injection for `IUserFacade`. Added `Create` and `Edit` endpoints to handle user creation and editing via HTTP POST and PUT requests, respectively. Introduced `EditUserViewModel` to encapsulate user editing data. Updated `Shop.Api.csproj` to fix formatting for a package reference.

--- Shop/Shop.Api/Controllers/UserController.cs
+++ Shop/Shop.Api/Controllers/UserController.cs
@@ -1,8 +1,26 @@
 ﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Api.ViewModels.User;
+using Shop.Application.Users.Create;
+using Shop.Application.Users.Edit;
+using Shop.Presentation.Facade.Users;
 
 namespace Shop.Api.Controllers
 {
-    public class UserController : ApiController
+    public class UserController(IUserFacade userFacade) : ApiController
     {
+        [HttpPost]
+        public async Task<ApiResult<Guid>> Create(CreateUserCommand command)
+        {
+            var result = await userFacade.Create(command);
+            return CommandResult(result);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ApiResult> Edit(Guid id, EditUserViewModel vm)
+        {
+            var result = await userFacade.Edit(new EditUserCommand(id, vm.Name, vm.Family, vm.PhoneNumber, vm.Email, vm.Gender));
+            return CommandResult(result);
+        }
     }
 }
---
+++ Shop/Shop.Api/ViewModels/User/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Domain.UserAgg.Enums;
+
+namespace Shop.Api.ViewModels.User
+{
+    public class EditUserViewModel
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
