d51c1a25d10090cdb22388d19af3603bd80ab507
Mojtaba Karamlou <contact@karamlou.com>
2025-09-20 00:28:12 +0330

    Add GetList endpoint to UserAddressController
    Updated `UserAddressController` to include a constructor with `IUserAddressFacade` dependency. Added a new `HttpGet` action method `GetList` to retrieve a list of user addresses for the current user. Updated `using` directives to include necessary namespaces for the new functionality.

--- Shop/Shop.Api/Controllers/UserAddressController.cs
+++ Shop/Shop.Api/Controllers/UserAddressController.cs
@@ -1,8 +1,16 @@
 ﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Presentation.Facade.Users.Addresses;
+using Shop.Query.Users.DTOs;
 
 namespace Shop.Api.Controllers;
 
-public class UserAddressController : ApiController
+public class UserAddressController(IUserAddressFacade addressFacade) : ApiController
 {
-    
+    [HttpGet]
+    public async Task<ApiResult<List<UserAddressDto>>> GetList()
+    {
+        var result = await addressFacade.GetList(User.GetUserId());
+        return QueryResult(result);
+    }
 }
