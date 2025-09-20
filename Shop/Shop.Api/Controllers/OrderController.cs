90e1d39b9c84e9142f56a2a062cb4e44a4d9c13d
Mojtaba Karamlou <contact@karamlou.com>
2025-09-21 00:57:47 +0330

    Add endpoints to OrderController for order queries
    Updated OrderController to use constructor injection with IOrderFacade.
    Added three new HTTP GET endpoints:
    1. GetByFilter: Retrieves orders based on filter parameters.
    2. GetById: Fetches an order by its GUID.
    3. GetCurrent: Retrieves the current user's order.
    
    Updated using directives to include necessary namespaces for
    facade and DTOs. These changes enhance the controller's
    functionality for querying orders.

--- Shop/Shop.Api/Controllers/OrderController.cs
+++ Shop/Shop.Api/Controllers/OrderController.cs
@@ -1,8 +1,31 @@
 ﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Presentation.Facade.Orders;
+using Shop.Query.Orders.DTOs;
+using Shop.Query.Orders.DTOs.Filter;
 
 namespace Shop.Api.Controllers;
 
-public class OrderController : ApiController
+public class OrderController(IOrderFacade orderFacade) : ApiController
 {
-    
+    [HttpGet]
+    public async Task<ApiResult<OrderFilterResult?>> GetByFilter([FromQuery] OrderFilterParams filters)
+    {
+        var result = await orderFacade.GetByFilter(filters);
+        return QueryResult(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ApiResult<OrderDto?>> GetById(Guid id)
+    {
+        var result = await orderFacade.GetById(id);
+        return QueryResult(result);
+    }
+
+    [HttpGet("current")]
+    public async Task<ApiResult<OrderDto?>> GetCurrent()
+    {
+        var result = await orderFacade.GetCurrentUserOrder(User.GetUserId());
+        return QueryResult(result);
+    }
 }
