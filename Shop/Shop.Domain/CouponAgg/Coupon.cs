52af572e6db8758411d9c22b558049b1b4262146
Mojtaba Karamlou <contact@karamlou.com>
2025-09-22 19:33:22 +0330

    Add Coupon aggregate and update project structure
    Introduced a new `Coupon` class in the `Shop.Domain.CouponAgg` namespace to represent discount coupons. The class includes properties for code, discount type, discount amount, expiration date, usage limit, and usage count. Added methods to validate coupon usage and mark it as used, with exception handling for invalid coupons.
    
    Updated `Shop.Domain.csproj` to include a new folder reference `CouponAgg\Repository\` in the project structure. Added necessary `using` directives for dependencies.

---
+++ Shop/Shop.Domain/CouponAgg/Coupon.cs
@@ -0,0 +1,41 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Common.Domain.Utils;
+using Shop.Domain._SharedKernel.Enums;
+
+namespace Shop.Domain.CouponAgg;
+
+public class Coupon : AggregateRoot
+{
+    public string Code { get; private set; }
+    public DiscountType DiscountType { get; private set; }
+    public int DiscountAmount { get; private set; }
+    public DateTime ExpirationDate { get; private set; }
+    public int UsageLimit { get; private set; }
+    public int UsedCount { get; private set; }
+
+    public Coupon(string code, DiscountType discountType, int discountAmount, DateTime expirationDate, int usageLimit)
+    {
+        Code = code;
+        DiscountType = discountType;
+        DiscountAmount = discountAmount;
+        ExpirationDate = expirationDate;
+        UsageLimit = usageLimit;
+    }
+
+    public bool IsValid() =>
+        DateTime.UtcNow <= ExpirationDate && UsedCount < UsageLimit;
+
+    public void MarkAsUsed()
+    {
+        if (!IsValid())
+            throw new InvalidDomainDataException("کد تخفیف معتبر نمیباشد");
+
+        UsedCount++;
+    }
+
+    private void Guard(string code, DiscountType discountType, int discountAmount, DateTime expirationDate, int usageLimit)
+    {
+
+    }
+}
