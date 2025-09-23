d248e288b1c151db7de1f44974c14af7694bd383
Mojtaba Karamlou <contact@karamlou.com>
2025-09-24 02:47:01 +0330

    Add DeleteCouponCommand and handler for coupon deletion
    Introduced `DeleteCouponCommand` and `DeleteCouponCommandHandler`
    in the `Shop.Application.Coupons.Delete` namespace to handle
    coupon deletion. The command encapsulates the `CouponId`, and
    the handler implements the deletion logic using `ICouponRepository`
    and `IUnitOfWork`.
    
    Updated `Shop.Application.csproj` to remove the `Coupons\Delete\`
    folder reference and added necessary project references.
    
    Included `using` directives for shared application utilities.

---
+++ Shop/Shop.Application/Coupons/Delete/DeleteCouponCommand.cs
@@ -0,0 +1,5 @@
+﻿using Common.Application;
+
+namespace Shop.Application.Coupons.Delete;
+
+public record DeleteCouponCommand(Guid CouponId) : IBaseCommand;
---
+++ Shop/Shop.Application/Coupons/Delete/DeleteCouponCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Common.Application;
+using Common.Application.OperationResults;
+using Common.Application.UnitOfWork;
+using Shop.Domain.CouponAgg.Repository;
+
+namespace Shop.Application.Coupons.Delete;
+
+public class DeleteCouponCommandHandler(ICouponRepository couponRepository, IUnitOfWork unitOfWork) : IBaseCommandHandler<DeleteCouponCommand>
+{
+    public async Task<OperationResult> Handle(DeleteCouponCommand request, CancellationToken cancellationToken)
+    {
+        var coupon = await couponRepository.GetByIdAsync(request.CouponId);
+        if (coupon == null) return OperationResult.NotFound();
+        couponRepository.Delete(coupon);
+        await unitOfWork.SaveChangesAsync();
+        return OperationResult.Success();
+    }
+}
