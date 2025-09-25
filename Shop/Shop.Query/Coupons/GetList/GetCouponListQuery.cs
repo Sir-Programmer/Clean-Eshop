f699efefe35e2114f51508e61f3a85fab583b255
Mojtaba Karamlou <contact@karamlou.com>
2025-09-26 01:07:43 +0330

    Add GetCouponList query and handler for coupon retrieval
    Introduced a new `GetCouponListQuery` to retrieve a list of
    coupons as `CouponDto` objects. Added a corresponding query
    handler `GetCouponListQueryHandler` that uses Entity Framework
    to fetch and map coupon data asynchronously.
    
    Updated `Shop.Query.csproj` to reflect folder structure changes
    by removing the `Coupons\GetList\` folder reference.

---
+++ Shop/Shop.Query/Coupons/GetList/GetCouponListQuery.cs
@@ -0,0 +1,6 @@
+﻿using Common.Query;
+using Shop.Query.Coupons.DTOs;
+
+namespace Shop.Query.Coupons.GetList;
+
+public record GetCouponListQuery : IQuery<List<CouponDto>>;
---
+++ Shop/Shop.Query/Coupons/GetList/GetCouponListQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Coupons.DTOs;
+
+namespace Shop.Query.Coupons.GetList;
+
+public class GetCouponListQueryHandler(ShopContext context) : IQueryHandler<GetCouponListQuery, List<CouponDto>>
+{
+    public async Task<List<CouponDto>> Handle(GetCouponListQuery request, CancellationToken cancellationToken)
+    {
+        return await context.Coupons.Select(c => c.Map()).ToListAsync(cancellationToken);
+    }
+}
