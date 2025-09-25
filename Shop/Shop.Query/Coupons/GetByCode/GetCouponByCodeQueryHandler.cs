ab352b1b54ecd6cc6f09319cafa7192d09f8f89c
Mojtaba Karamlou <contact@karamlou.com>
2025-09-26 01:08:42 +0330

    Refactor GetCouponByCodeQuery and its handler
    Split GetCouponByCodeQuery and GetCouponByCodeQueryHandler into
    separate files for better organization and maintainability.
    Removed unnecessary `using` directives from
    GetCouponByCodeQuery.cs. Created a new file,
    GetCouponByCodeQueryHandler.cs, to house the handler with
    appropriate `using` directives. No changes were made to the
    logic or functionality of the handler.

---
+++ Shop/Shop.Query/Coupons/GetByCode/GetCouponByCodeQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Coupons.DTOs;
+
+namespace Shop.Query.Coupons.GetByCode;
+
+public class GetCouponByCodeQueryHandler(ShopContext context) : IQueryHandler<GetCouponByCodeQuery, CouponDto?>
+{
+    public async Task<CouponDto?> Handle(GetCouponByCodeQuery request, CancellationToken cancellationToken)
+    {
+        var coupon = await context.Coupons.FirstOrDefaultAsync(c => c.Code == request.CouponCode, cancellationToken);
+        return coupon.MapOrNull();
+    }
+}
