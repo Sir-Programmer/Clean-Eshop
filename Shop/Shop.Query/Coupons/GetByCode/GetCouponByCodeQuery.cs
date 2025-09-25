22eed49e2bae9552fe49b9a328d1c4bc637f8213
Mojtaba Karamlou <contact@karamlou.com>
2025-09-26 00:59:41 +0330

    Add GetCouponByCode query handler and update project file
    Removed unused `<Folder Include="Coupons\GetByCode\" />` entry from `Shop.Query.csproj`.
    Added a new `GetCouponByCodeQuery` record and its handler in `GetCouponByCodeQuery.cs` to retrieve coupons by code.
    The handler uses Entity Framework to query the database and maps the result to a `CouponDto` or returns null if not found.

---
+++ Shop/Shop.Query/Coupons/GetByCode/GetCouponByCodeQuery.cs
@@ -0,0 +1,17 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Coupons.DTOs;
+
+namespace Shop.Query.Coupons.GetByCode;
+
+public record GetCouponByCodeQuery(string CouponCode) : IQuery<CouponDto?>;
+
+public class GetCouponByCodeQueryHandler(ShopContext context) : IQueryHandler<GetCouponByCodeQuery, CouponDto?>
+{
+    public async Task<CouponDto?> Handle(GetCouponByCodeQuery request, CancellationToken cancellationToken)
+    {
+        var coupon = await context.Coupons.FirstOrDefaultAsync(c => c.Code == request.CouponCode, cancellationToken: cancellationToken);
+        return coupon.MapOrNull();
+    }
+}
