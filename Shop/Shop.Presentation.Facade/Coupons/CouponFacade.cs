86116e4631e39745342dc7e8dbf8de08d343a8b1
Mojtaba Karamlou <contact@karamlou.com>
2025-09-26 01:25:30 +0330

    Implement CouponFacade with full ICouponFacade support
    Refactored the `CouponFacade` class to implement the `ICouponFacade` interface, providing concrete methods for creating, editing, deleting, using, and retrieving coupons. Introduced dependency injection for `IMediator` to handle commands and queries. Updated the `ICouponFacade` interface to define method signatures for all operations. Added necessary `using` directives for supporting namespaces.

--- Shop/Shop.Presentation.Facade/Coupons/CouponFacade.cs
+++ Shop/Shop.Presentation.Facade/Coupons/CouponFacade.cs
@@ -1,6 +1,54 @@
-﻿namespace Shop.Presentation.Facade.Coupons;
+﻿using AngleSharp.Dom;
+using Common.Application.OperationResults;
+using MediatR;
+using Shop.Application.Comments.Delete;
+using Shop.Application.Coupons.Create;
+using Shop.Application.Coupons.Edit;
+using Shop.Application.Coupons.Use;
+using Shop.Query.Coupons.DTOs;
+using Shop.Query.Coupons.GetByCode;
+using Shop.Query.Coupons.GetById;
+using Shop.Query.Coupons.GetList;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
-public class CouponFacade : ICouponFacade
+namespace Shop.Presentation.Facade.Coupons;
+
+public class CouponFacade(IMediator mediator) : ICouponFacade
 {
-    
+    public async Task<OperationResult<Guid>> Create(CreateCouponCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditCouponCommand command)
+    {
+        return await mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Delete(Guid id)
+    {
+        return await mediator.Send(new DeleteCommentCommand(id));
+    }
+
+    public async Task<OperationResult> Use(string code)
+    {
+        var coupon = await GetByCode(code);
+        if (coupon == null) return OperationResult.NotFound();
+        return await mediator.Send(new UseCouponCommand(coupon.Id));
+    }
+
+    public async Task<CouponDto?> GetById(Guid id)
+    {
+        return await mediator.Send(new GetCouponByIdQuery(id));
+    }
+
+    public async Task<CouponDto?> GetByCode(string code)
+    {
+        return await mediator.Send(new GetCouponByCodeQuery(code));
+    }
+
+    public async Task<List<CouponDto>> GetList()
+    {
+        return await mediator.Send(new GetCouponListQuery());
+    }
 }
--- Shop/Shop.Presentation.Facade/Coupons/ICouponFacade.cs
+++ Shop/Shop.Presentation.Facade/Coupons/ICouponFacade.cs
@@ -1,6 +1,18 @@
-﻿namespace Shop.Presentation.Facade.Coupons;
+﻿using Common.Application.OperationResults;
+using Shop.Application.Coupons.Create;
+using Shop.Application.Coupons.Edit;
+using Shop.Query.Coupons.DTOs;
+
+namespace Shop.Presentation.Facade.Coupons;
 
 public interface ICouponFacade
 {
-    
+    Task<OperationResult<Guid>> Create(CreateCouponCommand command);
+    Task<OperationResult> Edit(EditCouponCommand command);
+    Task<OperationResult> Delete(Guid id);
+    Task<OperationResult> Use(string code);
+
+    Task<CouponDto?> GetById(Guid id);
+    Task<CouponDto?> GetByCode(string code);
+    Task<List<CouponDto>> GetList();
 }
