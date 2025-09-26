d224c1da09fe0c298121057fc545db2a97853ae6
Mojtaba Karamlou <contact@karamlou.com>
2025-09-27 01:11:58 +0330

    Add CouponController with GET endpoints
    Introduced a new `CouponController` in the `Shop.Api.Controllers` namespace to handle coupon-related operations.
    
    - Added `using` directives for required namespaces.
    - Injected `ICouponFacade` dependency into the controller.
    - Implemented `GetList` endpoint to fetch all coupons.
    - Implemented `GetById` endpoint to fetch a coupon by its `id`.
    - Implemented an overloaded `GetById` endpoint to fetch a coupon by its `code`.

---
+++ Shop/Shop.Api/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Presentation.Facade.Coupons;
+using Shop.Query.Coupons.DTOs;
+
+namespace Shop.Api.Controllers;
+
+public class CouponController(ICouponFacade couponFacade) : ApiController
+{
+    [HttpGet]
+    public async Task<ApiResult<List<CouponDto>>> GetList()
+    {
+        var result = await couponFacade.GetList();
+        return QueryResult(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ApiResult<CouponDto?>> GetById(Guid id)
+    {
+        var result = await couponFacade.GetById(id);
+        return QueryResult(result);
+    }
+
+    [HttpGet("code/{code}")]
+    public async Task<ApiResult<CouponDto?>> GetById(string code)
+    {
+        var result = await couponFacade.GetByCode(code);
+        return QueryResult(result);
+    }
+}
