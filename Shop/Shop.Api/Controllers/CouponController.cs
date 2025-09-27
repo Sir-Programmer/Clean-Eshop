17dd566f1b432242b6c3e8d20fdcb50a8f71b65a
Mojtaba Karamlou <contact@karamlou.com>
2025-09-27 13:51:53 +0330

    Add CRUD endpoints for coupons and EditCouponViewModel
    Added new endpoints in CouponController for creating, editing, and deleting coupons. Introduced a new EditCouponViewModel class to handle coupon editing with properties such as Code, DiscountType, DiscountAmount, ExpirationDate, and UsageLimit. Updated using directives to include necessary namespaces.

--- Shop/Shop.Api/Controllers/CouponController.cs
+++ Shop/Shop.Api/Controllers/CouponController.cs
@@ -1,5 +1,9 @@
-﻿using Common.AspNetCore;
+﻿using System.Net;
+using Common.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Api.ViewModels.Coupon;
+using Shop.Application.Coupons.Create;
+using Shop.Application.Coupons.Edit;
 using Shop.Presentation.Facade.Coupons;
 using Shop.Query.Coupons.DTOs;
 
@@ -26,5 +30,27 @@ public class CouponController(ICouponFacade couponFacade) : ApiController
     {
         var result = await couponFacade.GetByCode(code);
         return QueryResult(result);
+    }
+
+    [HttpPost]
+    public async Task<ApiResult<Guid>> Create(CreateCouponCommand command)
+    {
+        var result = await couponFacade.Create(command);
+        var url = Url.Action("GetById", "Coupon", new { Id = result.Data }, Request.Scheme);
+        return CommandResult(result, statusCode: HttpStatusCode.Created, locationUrl: url);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ApiResult> Edit(Guid id, EditCouponViewModel vm)
+    {
+        var result = await couponFacade.Edit(new EditCouponCommand(id, vm.Code, vm.DiscountType, vm.DiscountAmount, vm.ExpirationDate, vm.UsageLimit));
+        return CommandResult(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ApiResult> Delete(Guid id)
+    {
+        var result = await couponFacade.Delete(id);
+        return CommandResult(result);
     }
 }
---
+++ Shop/Shop.Api/ViewModels/Coupon/EditCouponViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Domain._SharedKernel.Enums;
+
+namespace Shop.Api.ViewModels.Coupon;
+
+public class EditCouponViewModel
+{
+    public string Code { get; set; }
+    public DiscountType DiscountType { get; set; }
+    public int DiscountAmount { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public int UsageLimit { get; set; }
+}
